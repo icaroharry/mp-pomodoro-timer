a3484326a049b1baa3bf0653cd49084e2934d9ba
Ícaro Harry <icaro.harry@toptal.com>
2024-01-03 18:30:47 -0300

    Finish pomodoro project

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,1 +1,65 @@
-// Faça a lógica referente o cronômetro!
+const pomodoroSelect = document.querySelector("#pomodoro");
+const shortBreakSelect = document.querySelector("#short-break");
+const longBreakSelect = document.querySelector("#long-break");
+const startButton = document.querySelector("#start");
+const timerParagraph = document.querySelector("#counter");
+
+let selectedTimer = "pomodoro"; // pomodoro, short-break, long-break
+
+function changeSelectClasses(timer) {
+  if (timer === "pomodoro") {
+    pomodoroSelect.classList.add("active-button");
+    shortBreakSelect.classList.remove("active-button");
+    longBreakSelect.classList.remove("active-button");
+  } else if (timer === "short-break") {
+    pomodoroSelect.classList.remove("active-button");
+    shortBreakSelect.classList.add("active-button");
+    longBreakSelect.classList.remove("active-button");
+  } else if (timer === "long-break") {
+    pomodoroSelect.classList.remove("active-button");
+    shortBreakSelect.classList.remove("active-button");
+    longBreakSelect.classList.add("active-button");
+  }
+}
+
+function secondsToMinutesSeconds(totalSeconds) {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const padSeconds = seconds.toString().padStart(2, "0");
+  return `${minutes}:${padSeconds}`;
+}
+
+function getTimerValue(timer) {
+  return {
+    pomodoro: 25 * 60,
+    "short-break": 5 * 60,
+    "long-break": 25 * 60,
+  }[timer];
+}
+
+function changeTimerValue(timer) {
+  timerParagraph.textContent = secondsToMinutesSeconds(getTimerValue(timer));
+}
+
+function selectTimer(timer) {
+  selectedTimer = timer;
+
+  changeSelectClasses(timer);
+  changeTimerValue(timer);
+}
+
+function startTimer(timer) {
+  let seconds = getTimerValue(timer);
+
+  const interval = setInterval(() => {
+    seconds--;
+
+    timerParagraph.textContent = secondsToMinutesSeconds(seconds);
+    document.title = `${secondsToMinutesSeconds(seconds)} - Pomodoro`;
+
+    if (seconds === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,1 +1,65 @@
-// Faça a lógica referente as tarefas aqui!
+const tasksOl = document.querySelector("#tasks");
+const taskInput = document.querySelector("#new-task");
+const addTaskButton = document.querySelector("#add-task");
+const taskTemplate = document.querySelector("#task-template");
+
+renderTasksFromLocalStorage();
+
+addTaskButton.addEventListener("click", () => {
+  const newTask = taskInput.value;
+
+  addTask(newTask);
+});
+
+function renderTask(newTask) {
+  const taskTemplateClone = taskTemplate.content.cloneNode(true);
+  const newTaskElement = taskTemplateClone.querySelector(".task");
+  const taskText = newTaskElement.querySelector(".task-text");
+
+  const deleteButton = taskTemplateClone.querySelector("#delete-button");
+  deleteButton.addEventListener("click", () => deleteTask(newTask.id));
+
+  newTaskElement.id = newTask.id;
+  taskText.textContent = newTask.text;
+
+  tasksOl.appendChild(taskTemplateClone);
+}
+
+function saveTaskToLocalStorage(newTask) {
+  const tasks = localStorage.getItem("tasks");
+  const parsedTasks = JSON.parse(tasks) || [];
+
+  parsedTasks.push(newTask);
+
+  localStorage.setItem("tasks", JSON.stringify(parsedTasks));
+}
+
+function renderTasksFromLocalStorage() {
+  tasksOl.innerHTML = "";
+
+  const tasks = localStorage.getItem("tasks");
+  const parsedTasks = JSON.parse(tasks) || [];
+
+  parsedTasks.forEach((task) => renderTask(task));
+}
+
+function addTask(task) {
+  const newTask = {
+    id: Math.random().toString(16).slice(2),
+    text: task,
+  };
+
+  renderTask(newTask);
+  saveTaskToLocalStorage(newTask);
+}
+
+function deleteTask(taskId) {
+  const taskToDelete = document.getElementById(taskId);
+  taskToDelete.remove();
+
+  const tasks = localStorage.getItem("tasks");
+  const parsedTasks = JSON.parse(tasks) || [];
+
+  const filteredTasks = parsedTasks.filter((task) => task.id !== taskId);
+  localStorage.setItem("tasks", JSON.stringify(filteredTasks));
+}
